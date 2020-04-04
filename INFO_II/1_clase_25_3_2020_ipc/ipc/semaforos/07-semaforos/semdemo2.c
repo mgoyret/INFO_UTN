4be17d47b0c0baeb9d5f83791319fdb48a4be2c2
Marcos Goyret <marcosgoyret@gmail.com>
2020-04-04 20:11:08 -0300

    varios

--- INFO_II/1_clase_25_3_2020_ipc/ipc/semaforos/07-semaforos/semdemo2.c
+++ INFO_II/1_clase_25_3_2020_ipc/ipc/semaforos/07-semaforos/semdemo2.c
@@ -24,99 +24,101 @@ union semun {
 */
 int initsem(key_t key, int nsems)  /* key from ftok() */
 {
-	int i;
-	union semun arg;
-	struct semid_ds buf;
 	struct sembuf sb;
 	int semid;
 
 	semid = semget(key, nsems, IPC_CREAT | IPC_EXCL | 0666);
 
-	if (semid >= 0) { /* we got it first */
-		sb.sem_op = 1; sb.sem_flg = 0;
-		arg.val = 1;
+	if (semid >= 0) 
+	{ /* we got it first */
+		sb.sem_op = 1; 
+		sb.sem_flg = 0;
 
-		printf("press return\n"); getchar();
-
-		for(sb.sem_num = 0; sb.sem_num < nsems; sb.sem_num++) { 
+		for(sb.sem_num = 0; sb.sem_num < nsems; sb.sem_num++) 
+		{ 
 			/* do a semop() to "free" the semaphores. */
 			/* this sets the sem_otime field, as needed below. */
-			if (semop(semid, &sb, 1) == -1) {
+			if (semop(semid, &sb, 1) == -1) 
+			{
 				int e = errno;
 				semctl(semid, 0, IPC_RMID); /* clean up */
 				errno = e;
 				return -1; /* error, check errno */
 			}
 		}
+		return semid;
+	} 
 
-	} else if (errno == EEXIST) { /* someone else got it first */
-		int ready = 0;
-
+	if (errno == EEXIST) 
+	{ /* someone else got it first */
 		semid = semget(key, nsems, 0); /* get the id */
-		if (semid < 0) return semid; /* error, check errno */
-
-		/* wait for other process to initialize the semaphore: */
-		arg.buf = &buf;
-		for(i = 0; i < MAX_RETRIES && !ready; i++) {
-			semctl(semid, nsems-1, IPC_STAT, arg);
-			if (arg.buf->sem_otime != 0) {
-				ready = 1;
-			} else {
-				sleep(1);
-			}
-		}
-		if (!ready) {
-			errno = ETIME;
-			return -1;
-		}
-	} else {
-		return semid; /* error, check errno */
+		if (semid >= 0) 
+			return semid; 
 	}
 
-	return semid;
+return -1;
 }
 
-int main(void)
+int main(int argc, char *argv[])
 {
 	key_t key;
 	int semid;
 	struct sembuf sb;
-	
-	sb.sem_num = 0;
-	sb.sem_op = -1;  /* set to allocate resource */
-	sb.sem_flg = SEM_UNDO;
 
+
+//----------------------------------------------- Argumentos
+	if (argc<3)
+	{
+		printf ("\n ERROR:\n\t *** Ingrese ./app (+/-)sema_val bit_mode(1 NOWAIT/2 UNDO)***\n");
+		return 0;
+	}
+	
+	// Nro de semaforo
+	sb.sem_num = 0;
+
+	// valor de operacion ==> sem_val
+	sb.sem_op =atoi(*(argv+1));
+	printf ("--> sem_op:  %d \n-->sem_flg:",sb.sem_op);
+	
+	// Flags
+	sb.sem_flg = 0;
+	if (atoi(*(argv+2))&0x01)
+	{
+		sb.sem_flg |= IPC_NOWAIT;
+		puts ("IPC_NOWAIT ");
+	}
+	if (atoi(*(argv+2))&0x02)
+	{
+		sb.sem_flg |= SEM_UNDO;
+		puts ("SEM_UNDO");
+	}
+	if (!sb.sem_flg)
+		puts ("EMPTY");
+		
+	
+//----------------------------------------------- toma semaforo
+	
 	if ((key = ftok("semdemo.c", 'J')) == -1) {
 		perror("ftok");
 		exit(1);
 	}
-
+		
 	/* grab the semaphore set created by seminit.c: */
 	if ((semid = initsem(key, 1)) == -1) {
 		perror("initsem");
 		exit(1);
 	}
 
-	printf("Press return to lock: ");
-	getchar();
-	printf("Trying to lock...\n");
+//----------------------------------------------- operacion
 
-	if (semop(semid, &sb, 1) == -1) {
+	printf ("\n ***>> %d \n",semctl(semid, 0, GETVAL, NULL));
+
+	if (semop(semid, &sb, 1) == -1) 
+	{
 		perror("semop");
 		exit(1);
 	}
-
-	printf("Locked.\n");
-	printf("Press return to unlock: ");
-	getchar();
-
-	sb.sem_op = 1; /* free resource */
-	if (semop(semid, &sb, 1) == -1) {
-		perror("semop");
-		exit(1);
-	}
-
-	printf("Unlocked\n");
+	printf ("\n ***>> %d \n",semctl(semid, 0, GETVAL, NULL));
 
 	return 0;
 }
