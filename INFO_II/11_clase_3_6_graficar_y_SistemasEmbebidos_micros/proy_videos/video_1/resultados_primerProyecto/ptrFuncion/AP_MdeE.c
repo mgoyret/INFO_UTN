5227589bed7879717b93477a36725978781beb5a
Marcos Goyret <marcosgoyret@gmail.com>
2020-06-11 13:19:23 -0300

    s

---
+++ INFO_II/11_clase_3_6_graficar_y_SistemasEmbebidos_micros/proy_videos/video_1/resultados_primerProyecto/ptrFuncion/AP_MdeE.c
@@ -0,0 +1,127 @@
+
+/************************************************************************************************************************************
+ * La Redistribución y uso en formas de fuente y objeto con o sin modificación, están permitidos siempre que se cumplan las 
+ * siguientes condiciones:
+ *
+ * 1. Las redistribuciones del código fuente deben conservar el aviso de copyright anterior, esta lista de condiciones y el siguiente
+ * descargo de responsabilidad.
+ *
+ * 2. Las redistribuciones en formato objeto deben reproducir el aviso de copyright anterior, esta lista de condiciones y la siguiente
+ * exención de responsabilidad en la documentación y / u otros materiales proporcionados con la distribución.
+ *
+ * 3. Ni el nombre del titular de los derechos de autor ni los nombres de sus los contribuyentes pueden ser utilizados para respaldar 
+ * o promocionar productos derivados de este software sin permiso escrito previo específico.
+ *
+ * ESTE SOFTWARE ES PROPORCIONADO POR LOS TITULARES DE LOS DERECHOS DE AUTOR Y SUS COLABORADORES de uModelFactor y Y SE RENUNCIA 
+ * A CUALQUIER GARANTÍA EXPRESA O IMPLÍCITA, INCLUIDAS, ENTRE OTRAS, LAS GARANTÍAS IMPLÍCITAS DE COMERCIABILIDAD E IDONEIDAD PARA 
+ * UN PROPÓSITO DETERMINADO. EN NINGÚN CASO EL TITULAR DE LOS DERECHOS DE AUTOR O LOS COMERCIANTES SERÁN RESPONSABLES DE DAÑOS DIRECTOS,
+ * INDIRECTOS, INCIDENTALES, ESPECIALES, EJEMPLARES O CONSECUENCIALES (INCLUIDOS, ENTRE OTROS, LA ADQUISICIÓN DE BIENES O SERVICIOS 
+ * SUSTITUTIVOS, LA PÉRDIDA DE USO, LOS DATOS O LAS GANANCIAS; O INTERRUPCIÓN DEL NEGOCIO) SIN EMBARGO Y EN CUALQUIER TEORÍA DE 
+ * RESPONSABILIDAD, YA SEA POR CONTRATO, RESPONSABILIDAD ESTRICTA O AGRAVIO (INCLUIDA LA NEGLIGENCIA O CUALQUIER OTRO) QUE SURJA DE
+ * CUALQUIER MODO DEL USO DE ESTE SOFTWARE, AUN CUANDO SE ESTEVIENDO LA POSIBILIDAD DE DICHO DAÑo
+ *	
+ * El contenido de este archivo es generado en forma automatica a partir del modelo planteado. Cualquier cambio realizado sobre el modelo
+ * generara un nuevo codigo que eliminara todos los cambios realizados por el desarrollador en el fuente previamente generado
+**************************************************************************************************************************************/
+
+/**
+*	\file MdeE.c
+*	\brief 
+*	\details Descripcion detallada del archivo
+*	\author nosotros
+*	\date 03-06-2020 12:26:55
+*/
+/*********************************************************************************************************************************
+ *** INCLUDES
+**********************************************************************************************************************************/
+#include "AP_MdeE.h"
+#include "AP_FuncionesMdeE.h"
+#include "AP_Depuracion.h"
+
+/*********************************************************************************************************************************
+ *** DEFINES PRIVADOS AL MODULO
+**********************************************************************************************************************************/
+
+/*********************************************************************************************************************************
+ *** MACROS PRIVADAS AL MODULO
+**********************************************************************************************************************************/
+
+/*********************************************************************************************************************************
+ *** TIPOS DE DATOS PRIVADOS AL MODULO
+**********************************************************************************************************************************/
+
+/*********************************************************************************************************************************
+ *** TABLAS PRIVADAS AL MODULO
+**********************************************************************************************************************************/
+const void ( * arrayFunciones[ ] )( void ) = { funcion_APAGADO , funcion_ENCENDIDO };
+
+/*********************************************************************************************************************************
+ *** VARIABLES GLOBALES PUBLICAS
+**********************************************************************************************************************************/
+
+/*********************************************************************************************************************************
+ *** VARIABLES GLOBALES PRIVADAS AL MODULO
+**********************************************************************************************************************************/
+static int estado = APAGADO;
+
+/*********************************************************************************************************************************
+ *** PROTOTIPO DE FUNCIONES PRIVADAS AL MODULO
+**********************************************************************************************************************************/
+static void funcion_APAGADO( void );
+static void funcion_ENCENDIDO( void );
+
+/*********************************************************************************************************************************
+ *** FUNCIONES PRIVADAS AL MODULO
+**********************************************************************************************************************************/
+/**
+*	\fn static void funcion_APAGADO ( void )
+*	\brief Descripcion
+*	\details Detalles
+*	\author nosotros
+*	\date 03-06-2020 12:26:55
+*/
+static void funcion_APAGADO ( void )
+{
+	if( eTecla( ) )
+	{
+		 estado = ENCENDIDO;
+		 aEncenderLed(); 
+	}
+}
+
+/**
+*	\fn static void funcion_ENCENDIDO ( void )
+*	\brief Descripcion
+*	\details Detalles
+*	\author nosotros
+*	\date 03-06-2020 12:26:55
+*/
+static void funcion_ENCENDIDO ( void )
+{
+	if( eTecla( ) )
+	{
+		 estado = APAGADO;
+		 aApagarLed(); 
+	}
+}
+
+/*********************************************************************************************************************************
+ *** FUNCIONES GLOBALES AL MODULO
+**********************************************************************************************************************************/
+/**
+*	\fn void maquina_estado( void )
+*	\brief Maquina de estado mediante vector de ptr a funcion
+*	\details Detalles
+*	\author nosotros
+*	\date 03-06-2020 12:26:55
+*/
+void maquina_estado( void )
+{
+	if( estado > ENCENDIDO )
+	{
+		estado = APAGADO;
+		return;
+	}
+
+	( *arrayFunciones[ estado ] )( );
+}
